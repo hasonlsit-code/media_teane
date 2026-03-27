2e3df9722dfccb482a0cc255eb6080e99ff2aa6c
dquangdat04 <emailcuaban@gmail.com>
2026-03-27 13:48:59 +0700

    update deploy1

--- server/src/config/connectDB.js
+++ server/src/config/connectDB.js
@@ -5,6 +5,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("ket noi thanh cong");
   } catch (err) {
+  console.error("Loi ket noi MongoDB:", err.message);
     process.exit(1);
   }
 };
