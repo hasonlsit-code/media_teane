eb0771c3750ae97873632423236e93b5c9a75737
dquangdat04 <emailcuaban@gmail.com>
2026-03-27 15:24:41 +0700

    update frontend-zz

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
-import ChatBot from "./components/chatbot/ChatBot";
+import ChatBot from "./components/chatbot/Chatbot";
 import TeaMixerFloat from "./components/game/TeaMixerFloat";
 import Footer from "./components/Footer";
 
